c417cfc02a3f77f272df1d4d4ec4608d489d714a
Wandelite5 <123451343+Wandelite5@users.noreply.github.com>
2023-02-08 20:17:22 +0100

    updated project1 code

--- project1.py
+++ project1.py
@@ -1,29 +1,24 @@
-# Write a grading application that will read a list of student scores, Output the different grades and print out 
-# the class average score 
-print("Enter scores obtained in 6 subjects: ")
-student_score1 = int(input())
-student_score2 = int(input())
-student_score3 = int(input())
-student_score4 = int(input())
-student_score5 = int(input())
-student_score6 = int(input())
+def calculate_grades(scores):
+  grades = []
+  for score in scores:
+    if score >= 90:
+      grades.append("A")
+    elif score >= 85:
+      grades.append("B")
+    elif score >= 50:
+      grades.append("C")
+    else:
+      grades.append("F")
+  return grades
 
-total_score = student_score1 + student_score2 + student_score3 + student_score4 + student_score5 + student_score6
-class_average = round(total_score/6)
+def calculate_average(scores):
+  return round(sum(scores) / len(scores))
 
-# Grade A: >= 90
-# Grade B: >= 85 and < 90
-# Grade C: >= 50 and <85
-# Grade F:  < 50
+student_scores = [100, 10, 20, 90, 50, 88]
+student_grades = calculate_grades(student_scores)
+class_average = calculate_average(student_scores)
 
-if class_average >= 90:
-    print("Your Grade is A")
-elif class_average >= 85 and class_average < 90:
-    print("Your Grade is B")
-elif class_average >= 50 and class_average < 85:
-    print("Your Grade is C")
-else:
-    print("Your Grade is F")
-
-print("Your class average is {}".format(class_average))
+print("Student scores:", student_scores)
+print("Student grades:", student_grades)
+print("Class average score:", class_average)
 
